a4f4dd4ec2111bc9e117eaaf87ab1809fb60e156
meghdadFar <meghdad.farahmand@gmail.com>
2025-09-24 20:16:59 +0200

    New more generic base to make call to any provider

--- src/twelve_angry_llms/__init__.py
+++ src/twelve_angry_llms/__init__.py
@@ -1,2 +1,14 @@
-def hello() -> str:
-    return "Hello from twelve-angry-llms!"
+from .jury import Jury
+from .judge import Judge
+from .tasks import GenerationTask, ClassificationTask, RankingTask
+from .types import JuryResult, JudgeOutput
+
+__all__ = [
+    "Jury",
+    "Judge",
+    "GenerationTask",
+    "ClassificationTask",
+    "RankingTask",
+    "JuryResult",
+    "JudgeOutput",
+]
